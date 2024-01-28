14285d4e7033d3fbc9ee9351da357ce26913c782
sandeep jain <149592407+sandeepjain321@users.noreply.github.com>
2024-01-28 17:40:34 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+let showsidebar = () => {
+  const sidebar = document.querySelector(".sidebar");
+  sidebar.style.display = "flex";
+};
+
+let hidesidebar = () => {
+  const sidebar = document.querySelector(".sidebar");
+  sidebar.style.display = "none";
+};
